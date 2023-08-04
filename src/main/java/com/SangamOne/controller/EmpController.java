395d972e96ef0ba02ea7a3665ad3b20d742f2b91
PrajwalKadame <prajwalkadame.sangamone@gmail.com>
2023-08-04 10:04:53 +0530

    Emp System

---
+++ src/main/java/com/SangamOne/controller/EmpController.java
@@ -0,0 +1,53 @@
+package com.SangamOne.controller;
+
+import java.util.List;
+
+import org.apache.coyote.http11.Http11InputBuffer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner.Mode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.SangamOne.entity.Employee;
+import com.SangamOne.repository.EmpRepo;
+import com.SangamOne.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class EmpController {
+	@Autowired
+	EmpService empService;
+	
+	@GetMapping("/")
+	public String home(Model m) {
+		List<Employee> emp=empService.getAllEmp();
+		m.addAttribute("emp", emp);
+		return "Index";
+	}
+	
+	@GetMapping("/addemp")
+	public String addEmpForm() {
+		return "add_emp";
+	}
+	
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e, HttpSession session) {
+	System.out.println(e);
+	empService.addEmp(e);
+	session.setAttribute("msg", "Emp added successfully...");
+	return "redirect:/";
+	}
+	
+	@GetMapping("/edit{id}")
+	public String edit(@PathVariable int id, Model m) {
+		Employee e=empService.getEmpId(id);
+		m.addAttribute("emp", e);
+		return "edit";
+	}
+
+}
